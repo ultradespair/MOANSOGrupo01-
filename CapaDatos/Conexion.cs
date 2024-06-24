6e404d1f956bd314cc3d5522c3a01c256f582f5f
Walker2807 <alexleonreyes@gmail.com>
2024-06-23 23:10:28 -0500

    Agregue la conexion a la base de datos

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace CapaDatos
 {
     public class Conexion
     {
+        //patron de Diseño Singleton
+        private static readonly Conexion _instancia = new Conexion();
+        public static Conexion Instancia
+        {
+            get { return Conexion._instancia; }
+        }
 
+        public SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=.; Initial Catalog = DBCliente4;" +//"User ID=sa; Password=123";
+                                "Integrated Security=true";
 
-
-
+            return cn;
+        }
     }
 }
--- CapaEntidad/entCliente.cs
+++ CapaEntidad/entCliente.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace CapaEntidad
 {
-    public class Class1
+    public class entCliente
     {
+
+
+
+
     }
 }
